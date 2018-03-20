671c3a8cc398db5a58d9d10398692b69ac7c1938
niederle <niederle@PC545.SPSEJECNANET.CZ>
2018-03-20 11:21:55 +0100

    update main

--- XOR/src/Main.java
+++ XOR/src/Main.java
@@ -1,10 +1,16 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class Main {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -34,8 +40,32 @@ public class Main {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 375, 210);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Cesta k souboru:");
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 13));
+		lblNewLabel.setBounds(10, 47, 102, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(113, 45, 224, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnPotvrdit = new JButton("Potvrdit");
+		btnPotvrdit.setBounds(10, 138, 89, 23);
+		frame.getContentPane().add(btnPotvrdit);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(50, 96, 86, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Heslo:");
+		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 13));
+		lblNewLabel_1.setBounds(10, 95, 69, 20);
+		frame.getContentPane().add(lblNewLabel_1);
 	}
-
 }

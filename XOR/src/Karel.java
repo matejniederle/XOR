8821d5c55433da2e6014578c2f7d2e8297e3763b
karel <karel@PC544.SPSEJECNANET.CZ>
2018-03-20 11:17:49 +0100

    class Karel

---
+++ XOR/src/Karel.java
@@ -0,0 +1,32 @@
+
+public class Karel {
+
+	
+	private String cesta;
+	private String jmeno;
+	private String heslo;
+	
+	public Karel(String cesta , String jmeno , String heslo) {
+		this.cesta = cesta;
+		this.jmeno = jmeno;
+		this.heslo = heslo;
+	}
+	public String getxored() {
+		
+		
+		return null;
+	}
+	
+	public void shifra(){
+		this.cesta = getxored();
+		this.jmeno = getxored();
+		this.heslo = getxored();
+	}
+	
+	public String toString() {
+		return cesta+" "+jmeno+" "+heslo;
+	}
+	
+	
+	
+}
